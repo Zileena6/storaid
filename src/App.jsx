66cd7c9be02d6a83997bc1580ab548bfa33d7a74
Linda Leminaho <linda.leminaho@gmail.com>
2025-10-15 04:01:25 +0200

    Initial commit: Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Home, About, Services, Contact, Booking, Error } from './pages';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
