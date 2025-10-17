ce6a4cbf1b65bd68af191c024f47721f030ca45d
Linda Leminaho <linda.leminaho@gmail.com>
2025-10-18 01:43:11 +0200

    Adds SocialLinks Component

---
+++ src/components/social/links.js
@@ -0,0 +1,13 @@
+import {
+  FaFacebookF,
+  FaXTwitter,
+  FaInstagram,
+  FaYoutube,
+} from 'react-icons/fa6';
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.facebook.com', icon: FaFacebookF },
+  { id: 2, href: 'https://x.com', icon: FaXTwitter },
+  { id: 3, href: 'https://www.instagram.com', icon: FaInstagram },
+  { id: 4, href: 'https://www.youtube.com', icon: FaYoutube },
+];
