243840db83daacb951635c3d85fd8c9784909a6c
Linda Leminaho <linda.leminaho@gmail.com>
2025-10-18 17:13:03 +0200

    Adds ReviewCard component

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,35 @@
+import quotes from '../../public/quotes.svg';
+import { MdStar } from 'react-icons/md';
+
+const ReviewCard = () => {
+  return (
+    <div className='review-card'>
+      <div className='rating-stars'>
+        <MdStar />
+        <MdStar />
+        <MdStar />
+        <MdStar />
+      </div>
+
+      <p className='review-post'>
+        Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero
+        lectus, porta nec turpis sit amet, lobortis fringilla ante.
+      </p>
+
+      <div className='user-container'>
+        <div className='author-info'>
+          <div className='avatar'></div>
+
+          <div className='user'>
+            <h6>Aiden Harvey</h6>
+            <p>customer</p>
+          </div>
+        </div>
+        <div>
+          <img src={quotes} alt='yellow quotes icon' />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ReviewCard;
