4fc5143ff2093e55f8f045419741ee6a6c928fbb
Linda Leminaho <linda.leminaho@gmail.com>
2025-10-21 02:26:41 +0200

    Adds StatsCard component wip

---
+++ src/components/StatsCard.jsx
@@ -0,0 +1,21 @@
+// TODO: In progress...
+
+const StatsCard = () => {
+  return (
+    <div className='stats-container'>
+      <div className='block'>
+        <h2>12+</h2>
+        <p>years of experience</p>
+      </div>
+      <div className='block'>
+        <h2>150K+</h2>
+        <p>satisfied clients</p>
+      </div>
+      <div className='block'>
+        <h2>35+</h2>
+        <p>warehouse</p>
+      </div>
+    </div>
+  );
+};
+export default StatsCard;
