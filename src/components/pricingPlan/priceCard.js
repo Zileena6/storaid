4eb1743555171070f0f3ea75b16c9ee31b9504df
Linda Leminaho <linda.leminaho@gmail.com>
2025-11-10 16:53:39 +0100

    Deploy test

---
+++ src/components/pricingPlan/priceCard.js
@@ -0,0 +1,30 @@
+export const priceCard = [
+  {
+    id: 'small',
+    title: 'Small Unit',
+    price: '50',
+    description:
+      'Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.',
+  },
+  {
+    id: 'medium',
+    title: 'Medium Unit',
+    price: '100',
+    description:
+      'Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.',
+  },
+  {
+    id: 'large',
+    title: 'Large Unit',
+    price: '150',
+    description:
+      'Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.',
+  },
+  {
+    id: 'executive',
+    title: 'Executive Unit',
+    price: '200',
+    description:
+      'Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.',
+  },
+];
